c7e40149c0e227912290c8e375ee556f6205102d
Sujendran Reddy <ST10457761@vcconnect.edu.za>
2026-04-20 20:17:16 +0200

    Created the CurrencyServiceTest.

---
+++ GLMS.Tests/CurrencyServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using GLMS.Interfaces;
+using Xunit;
+
+namespace GLMS.Tests
+{
+    public class CurrencyServiceTests
+    {
+        [Fact]
+        public void ConvertUsdToZar_CalculatesCorrectAmount()
+        {
+            decimal usdAmount = 10m;
+            decimal exchangeRate = 18.50m;
+
+            decimal zarAmount = Math.Round(usdAmount * exchangeRate, 2);
+
+            Assert.Equal(185.00m, zarAmount);
+        }
+
+        [Fact]
+        public void ConvertUsdToZar_ZeroUsd_ReturnsZero()
+        {
+            decimal usdAmount = 0m;
+            decimal exchangeRate = 18.50m;
+
+            decimal zarAmount = Math.Round(usdAmount * exchangeRate, 2);
+
+            Assert.Equal(0m, zarAmount);
+        }
+
+        [Fact]
+        public void ConvertUsdToZar_RoundsToTwoDecimals()
+        {
+            decimal usdAmount = 12.345m;
+            decimal exchangeRate = 18.2789m;
+
+            decimal zarAmount = Math.Round(usdAmount * exchangeRate, 2);
+
+            Assert.Equal(Math.Round(12.345m * 18.2789m, 2), zarAmount);
+        }
+    }
+}
